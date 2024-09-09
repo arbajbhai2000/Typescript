71274f693ee5ada4764187cdfd658ee57a58e792
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-09-09 11:24:04 +0530

    Add files via upload

---
+++ conditionalstatement.js
@@ -0,0 +1,19 @@
+var v1 = 20;
+// if (v1==20){
+//     console.log('Ten');
+// }
+// else{
+// console.log('Not Ten');
+// }
+console.log('Program Ends');
+// let result1: string = (v1 == 10) ? 'Ten' : 'Not Ten';
+// console.log(result1);
+if (v1 == 10) {
+    console.log('Ten');
+}
+else if (v1 == 20) {
+    console.log('Twenty');
+}
+else {
+    console.log('Number Not Ten And Twenty');
+}
