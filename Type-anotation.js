ce40a347a8aa74dd5f70d5095425523418ad87df
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-09-12 15:42:21 +0530

    Add files via upload

---
+++ Type-anotation.js
@@ -0,0 +1,11 @@
+var num1 = 10;
+console.log(num1);
+var num2 = 'Arbaj';
+console.log(num2);
+var num3 = true;
+console.log(num3);
+function addition(a, b) {
+    return a + b;
+}
+console.log(addition(100, 200));
+console.log(addition('Arbaj', 10));
---
+++ Type-anotation.ts
@@ -0,0 +1,16 @@
+var num1: number=10;
+console.log(num1);
+
+var num2 :string='Arbaj';
+console.log(num2);
+
+var num3 =true;
+console.log(num3);
+
+function addition(a,b){
+    return a+b;
+}
+
+console.log(addition(100,200));
+
+console.log(addition('Arbaj' ,10));
